b02fa70bb7617e7ca0e20c549786f728a49f60cf
TranThibaut <thibaut.tran@epitech.eu>
2023-03-10 15:49:52 +0000

    push parser

--- app/Main.hs
+++ app/Main.hs
@@ -20,6 +20,7 @@ data Flags = Flags {
     window :: Maybe Int,
     move :: Maybe Int
 }
+
 -- instance Show Flags where
 --     show flags = "rule: " ++ show (rule flags) ++ "\n" ++ "start: " ++ show (start flags) ++ "\n" ++ "lines: " ++ show (nbLines flags) ++ "\n" ++ "window: " ++ show (window flags) ++ "\n" ++ "move: " ++ show (move flags)
 
@@ -40,15 +41,33 @@ mustFlags args =
     else exitWith (ExitFailure 84)
 
 --parser
+checkFlags3 :: [String] -> Flags -> Maybe Flags
+checkFlags3 (x:xs) flags = case x of
+    "--move" -> case xs of
+            [] -> Nothing
+            (m:ms) -> checkFlags ms (flags { move = (readMaybe m) })
+    _ -> Nothing
+
+checkFlags2 :: [String] -> Flags -> Maybe Flags
+checkFlags2 (x:xs) flags = case x of
+    "--lines" -> case xs of
+            [] -> Nothing
+            (l:ls) -> checkFlags ls (flags { nbLines = (readMaybe l) })
+    "--window" -> case xs of
+            [] -> Nothing
+            (w:ws) -> checkFlags ws (flags { window = (readMaybe w) })
+    _ -> checkFlags3 (x:xs) flags
+
 checkFlags :: [String] -> Flags -> Maybe Flags
 checkFlags [] flags = Just flags
 checkFlags (x:xs) flags = case x of
-    "--rule" -> checkFlags(tail xs) (flags { rule = (readMaybe(head xs)) })
-    "--start" -> checkFlags(tail xs) (flags { start = (readMaybe(head xs)) })
-    "--lines" -> checkFlags(tail xs) (flags { nbLines = (readMaybe(head xs)) })
-    "--window" -> checkFlags(tail xs) (flags { window = (readMaybe(head xs)) })
-    "--move" -> checkFlags(tail xs) (flags { move = (readMaybe(head xs)) })
-    _ -> Nothing
+    "--rule" -> case xs of
+            [] -> Nothing
+            (r:rs) -> checkFlags rs (flags { rule = (readMaybe r) })
+    "--start" -> case xs of
+            [] -> Nothing
+            (s:ss) -> checkFlags ss (flags { start = (readMaybe s) })
+    _ -> checkFlags2 (x:xs) flags
 
 -- converti un int en list de bits
 intToBits :: Int -> [Int]
